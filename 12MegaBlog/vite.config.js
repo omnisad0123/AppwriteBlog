db5009febf062abcf8ef70257adc26f4273bba72
om.prakashnisad.ece20@itbhu.ac.in <omnisad0123>
2024-05-20 22:42:54 +0530

    4rth

--- 12MegaBlog/vite.config.js
+++ 12MegaBlog/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the build output goes to the 'dist' directory
+  },
+  server: {
+    open: true, // Automatically opens the browser when the server starts
+  },
 })
